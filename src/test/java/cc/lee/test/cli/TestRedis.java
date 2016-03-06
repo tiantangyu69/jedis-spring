04fcd5c57a5db82ab1828d83deb5f2193af8322e
lizhitao <304663257@qq.com>
2016-03-06 21:26:49 +0800

    初始化项目

---
+++ src/test/java/cc/lee/test/cli/TestRedis.java
@@ -0,0 +1,14 @@
+package cc.lee.test.cli;
+
+import org.junit.Test;
+
+import redis.clients.jedis.Jedis;
+
+public class TestRedis {
+	@Test
+	public void testJedis() {
+		Jedis jedis = new Jedis("localhost");
+		jedis.set("foo", "cc.lee");
+		System.out.println(jedis.get("foo"));
+	}
+}
